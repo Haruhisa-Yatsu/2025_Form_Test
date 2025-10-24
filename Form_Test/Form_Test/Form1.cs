650e7d1a5246221a22583105e45cf7346e397c06
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-10-24 15:35:58 +0900

    練習問題１，２

---
+++ Form_Test/Form_Test/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    // インスタンスの生成
+                    TestButton testButton = new TestButton();
+
+                    // ボタンの位置を設定
+                    testButton.Location = new Point(50 * i, 50 * j);
+
+                    // ボタンの大きさを設定
+                    testButton.Size = new Size(50, 50);
+
+                    // ボタン内のテキストを設定
+                    testButton.Text = "";
+
+                    // コントロールにボタンを追加
+                    Controls.Add(testButton);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("(´・ω・｀)");
+        }
+    }
+}
---
+++ Form_Test/Form_Test/TestButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Test
+{
+    // Buttonクラスを継承した
+    // TestButtonクラスを作成してください。
+    internal class TestButton : Button
+    {
+
+    }
+}
