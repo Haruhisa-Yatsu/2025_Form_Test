2c00aae63d2edb83b93a11d3e07644ba99324b79
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-10-31 15:33:23 +0900

    TestButtonOnとOffの切り替え作成

--- Form_Test/Form_Test/TestButton.cs
+++ Form_Test/Form_Test/TestButton.cs
@@ -8,10 +8,34 @@ using System.Windows.Forms;
 
 namespace Form_Test
 {
-    // Buttonクラスを継承した
-    // TestButtonクラスを作成してください。
     internal class TestButton : Button
     {
+        /// <summary>onの時の色</summary>
+        private Color _onColor = Color.LightBlue;
+
+        /// <summary>offの時の色</summary>
+        private Color _offColor = Color.DarkBlue;
+
+        /// <summary>現在OnかOffか</summary>
+        private bool _enable;
+
+
+        /// <summary>onとoffの設定</summary> 
+        /// <param name="on"></param>
+        public void SetEnable(bool on)
+        {
+            _enable = on;
+            if (on)
+            {
+                BackColor = _onColor;
+            }
+            else
+            {
+                BackColor = _offColor;
+            }
+        }
+
+
         public TestButton(Point position, Size size, string text)
         {
             // ボタンの位置を設定
@@ -21,6 +45,8 @@ namespace Form_Test
             // ボタン内のテキストを設定
             Text = text;
 
+            SetEnable(false);
+
             Click += ClickEvent;
         }
 
@@ -28,7 +54,8 @@ namespace Form_Test
         // 自分で作成することも可能
         private void ClickEvent(object sender, EventArgs e)
         {
-            MessageBox.Show("TestButtonないで設定");
-        }
+
+            SetEnable(!_enable);
+        }   
     }
 }
